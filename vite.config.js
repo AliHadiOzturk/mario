06d82a4bc0249ba05c9bbcb2424ff07d54020102
Ali Hadi Öztürk <ozturkalihadi@gmail.com>
2023-03-13 00:07:30 +0300

    vite config relative base path added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import {defineConfig} from "vite"
 
 export default defineConfig({
+    base: "./",
     server: {
         port: 3500
     },
     build: {
-        outDir: "build"
+        outDir: "build",
     }
 })
